0df8d3d5752dccacfb8ded6acb9771bb81f4a451
307guojiawei@163.com <307guojiawei@163.com>
2018-05-18 20:35:23 +0000

    Modify a little

--- MainFrame.py
+++ MainFrame.py
@@ -4,16 +4,18 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 import App
+
+
 styleDir = "./assets/style.qss" #全局style文件路径
 
 
 def main():
     # 提升到root权限
-    if os.geteuid():
-        args = [sys.executable] + sys.argv
-        # 下面两种写法，一种使用su，一种使用sudo，都可以
-        #os.execlp('su', 'su', '-c', ' '.join(args))
-        os.execlp('sudo', 'sudo', *args)
+    # if os.geteuid():
+    #     args = [sys.executable] + sys.argv
+    #     # 下面两种写法，一种使用su，一种使用sudo，都可以
+    #     #os.execlp('su', 'su', '-c', ' '.join(args))
+    #     os.execlp('sudo', 'sudo', *args)
 
     app = QApplication(sys.argv)
     window = App.MyWindow()
