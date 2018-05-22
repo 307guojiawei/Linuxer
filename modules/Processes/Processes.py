5757b3040ed4d8e646dcf54bc39f10c95b37d021
EricMa <307748790@qq.com>
2018-05-22 16:23:23 +0800

    now the table can show all processes

--- modules/Processes/Processes.py
+++ modules/Processes/Processes.py
@@ -17,7 +17,6 @@ class ProcessesWidget(QtWidgets.QWidget, Ui_processForm):
         self.processModel = ProcessTableModel()
         self.processTable.setModel(self.processModel)
         self.myThread.sinOut.connect(self.updateProcessData)
-        self.processModel.dataChanged.connect(self.processTable.reset)
         self.processTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.processTable.show()
         self.selectedPID = None
@@ -35,8 +34,8 @@ class ProcessesWidget(QtWidgets.QWidget, Ui_processForm):
         self.lcdProcess.display(payload['count'])
 
     def selectProcess(self,index):
-        process = self.processModel.getRow(index)
-        self.selectedPID = int(process[0])
+        process = self.processModel.item(index.row(),0)
+        self.selectedPID = int(process.text())
         self.labelPID.setText("Selected PID: "+str(self.selectedPID))
 
     def killProcess(self):
@@ -45,77 +44,107 @@ class ProcessesWidget(QtWidgets.QWidget, Ui_processForm):
             tool.killProcess(self.selectedPID)
 
 
-
-
-class ProcessTableModel(QAbstractTableModel):
+class ProcessTableModel(QStandardItemModel):
     dataChanged = pyqtSignal()
 
 
     def __init__(self):
         super(ProcessTableModel, self).__init__()
-        self.data = list()
-        self.dirty = False
+        self.setHeaders()
 
-    def getRow(self,index):
-        if (not index.isValid() or
-                not (0 <= index.row() < len(self.data))):
-            return QVariant()
-        process = self.data[index.row()]
-        return process
+    def setHeaders(self):
+        self.setHorizontalHeaderLabels(["PID","Memory %","CPU %","User","Process"])
 
-    def data(self, index, role=Qt.DisplayRole):
-        if (not index.isValid() or
-                not (0 <= index.row() < len(self.data))):
-            return QVariant()
-        process = self.data[index.row()]
-        column = index.column()
-        if role == Qt.DisplayRole:
-            return process[column]
-
-    def headerData(self, section, orientation, role=Qt.DisplayRole):
-        if role == Qt.TextAlignmentRole:
-            if orientation == Qt.Horizontal:
-                return QVariant(int(Qt.AlignLeft | Qt.AlignVCenter))
-            return QVariant(int(Qt.AlignRight | Qt.AlignVCenter))
-        if role != Qt.DisplayRole:
-            return QVariant()
-        if orientation == Qt.Horizontal:
-            if section == 0:
-                return QVariant("PID")
-            elif section == 1:
-                return QVariant("Memory %")
-            elif section == 2:
-                return QVariant("CPU %")
-            elif section == 3:
-                return QVariant("User")
-            elif section == 4:
-                return QVariant("Process")
-        return QVariant(int(section + 1))
-
-    def rowCount(self, index=QModelIndex()):
-        return 100
-
-    def columnCount(self, index=QModelIndex()):
-        return 5
 
     def loadData(self,payload): #payload是一个list,其中item是dict类型
         payload = payload["res"]
-        res = list()
+        self.clear()
+        self.setHeaders()
         for item in payload:
             process = list()
-            process.append(item['pid'])
-            process.append(item['mem'])
-            process.append(item['cpu'])
-            process.append(item['user'])
-            process.append(item['name'])
-            res.append(process)
-        self.resetInternalData()
-        self.data = res
-        self.dirty = True
+            process.append(QStandardItem(item['pid']))
+            process.append(QStandardItem(item['mem']))
+            process.append(QStandardItem(item['cpu']))
+            process.append(QStandardItem(item['user']))
+            process.append(QStandardItem(item['name']))
+            self.appendRow(process)
+        #self.resetInternalData()
 
 
 
-        self.dataChanged.emit()
+        #self.dataChanged.emit()
+
+
+#
+# class ProcessTableModel(QAbstractTableModel):
+#     dataChanged = pyqtSignal()
+#
+#
+#     def __init__(self):
+#         super(ProcessTableModel, self).__init__()
+#         self.data = list()
+#         self.dirty = False
+#
+#     def getRow(self,index):
+#         if (not index.isValid() or
+#                 not (0 <= index.row() < len(self.data))):
+#             return QVariant()
+#         process = self.data[index.row()]
+#         return process
+#
+#     def data(self, index, role=Qt.DisplayRole):
+#         if (not index.isValid() or
+#                 not (0 <= index.row() < len(self.data))):
+#             return QVariant()
+#         process = self.data[index.row()]
+#         column = index.column()
+#         if role == Qt.DisplayRole:
+#             return process[column]
+#
+#     def headerData(self, section, orientation, role=Qt.DisplayRole):
+#         if role == Qt.TextAlignmentRole:
+#             if orientation == Qt.Horizontal:
+#                 return QVariant(int(Qt.AlignLeft | Qt.AlignVCenter))
+#             return QVariant(int(Qt.AlignRight | Qt.AlignVCenter))
+#         if role != Qt.DisplayRole:
+#             return QVariant()
+#         if orientation == Qt.Horizontal:
+#             if section == 0:
+#                 return QVariant("PID")
+#             elif section == 1:
+#                 return QVariant("Memory %")
+#             elif section == 2:
+#                 return QVariant("CPU %")
+#             elif section == 3:
+#                 return QVariant("User")
+#             elif section == 4:
+#                 return QVariant("Process")
+#         return QVariant(int(section + 1))
+#
+#     def rowCount(self, index=QModelIndex()):
+#         return 100
+#
+#     def columnCount(self, index=QModelIndex()):
+#         return 5
+#
+#     def loadData(self,payload): #payload是一个list,其中item是dict类型
+#         payload = payload["res"]
+#         res = list()
+#         for item in payload:
+#             process = list()
+#             process.append(item['pid'])
+#             process.append(item['mem'])
+#             process.append(item['cpu'])
+#             process.append(item['user'])
+#             process.append(item['name'])
+#             res.append(process)
+#         self.resetInternalData()
+#         self.data = res
+#         self.dirty = True
+#
+#
+#
+#         self.dataChanged.emit()
 
 
 
